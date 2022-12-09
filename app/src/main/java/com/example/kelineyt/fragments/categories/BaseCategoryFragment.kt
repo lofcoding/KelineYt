91ea684efeaa2087355dcc13cabe02da83409c89
mohammad <mohammednawas8@gmail.com>
2022-12-09 15:25:24 +0200

    E19

--- app/src/main/java/com/example/kelineyt/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/kelineyt/fragments/categories/BaseCategoryFragment.kt
@@ -1,33 +1,19 @@
 package com.example.kelineyt.fragments.categories
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kelineyt.R
 import com.example.kelineyt.adapters.BestProductsAdapter
-import com.example.kelineyt.data.Category
 import com.example.kelineyt.databinding.FragmentBaseCategoryBinding
-import com.example.kelineyt.util.Resource
-import com.example.kelineyt.viewmodel.CategoriesViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
-@AndroidEntryPoint
-open class BaseCategoryFragment(private val category: Category) :
-    Fragment(R.layout.fragment_base_category) {
+open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
     private lateinit var binding: FragmentBaseCategoryBinding
-    private val viewModel by activityViewModels<CategoriesViewModel>()
-
+    private lateinit var offerAdapter: BestProductsAdapter
     private lateinit var bestProductsAdapter: BestProductsAdapter
 
     override fun onCreateView(
@@ -42,49 +28,11 @@ open class BaseCategoryFragment(private val category: Category) :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.getProductsByCategory(category)
-
-        setupBestProducts()
-
-        lifecycleScope.launch {
-            viewModel.error.collect {
-                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        //Collect products
-        lifecycleScope.launchWhenStarted {
-            viewModel.categoriesProducts.collectLatest { productsCategories ->
-                binding.offerProductsProgressBar.visibility = View.GONE
-                when (category) {
-                    is Category.Chair -> {
-                        bestProductsAdapter.differ.submitList(productsCategories.chairProducts?.bestProducts)
-                    }
-                    is Category.Cupboard -> {
-                        bestProductsAdapter.differ.submitList(productsCategories.cupboardProducts?.bestProducts)
-                    }
-                    is Category.Table -> {
-                        bestProductsAdapter.differ.submitList(productsCategories.tableProducts?.bestProducts)
-                    }
-                    is Category.Accessory -> {
-                        bestProductsAdapter.differ.submitList(productsCategories.accessoryProducts?.bestProducts)
-                    }
-                    is Category.Furniture -> {
-                        bestProductsAdapter.differ.submitList(productsCategories.furnitureProducts?.bestProducts)
-                    }
-                }
-            }
-        }
-
-        //Collect loading
-        lifecycleScope.launchWhenStarted {
-            viewModel.loading.collectLatest {
-                binding.offerProductsProgressBar.visibility = if (it) View.VISIBLE else View.GONE
-            }
-        }
+        setupOfferRv()
+        setupBestProductsRv()
     }
 
-    private fun setupBestProducts() {
+    private fun setupBestProductsRv() {
         bestProductsAdapter = BestProductsAdapter()
         binding.rvBestProducts.apply {
             layoutManager =
@@ -92,5 +40,14 @@ open class BaseCategoryFragment(private val category: Category) :
             adapter = bestProductsAdapter
         }
     }
-}
 
+    private fun setupOfferRv() {
+        offerAdapter = BestProductsAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+            adapter = offerAdapter
+        }
+    }
+
+}
