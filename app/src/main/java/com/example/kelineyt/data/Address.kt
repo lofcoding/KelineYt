7c81e9bdfdf77399845067d1e24f2c248c3da7b4
mohammad <mohammednawas8@gmail.com>
2023-01-24 13:42:54 +0200

    E28

---
+++ app/src/main/java/com/example/kelineyt/data/Address.kt
@@ -0,0 +1,17 @@
+package com.example.kelineyt.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Address(
+    val addressTitle: String,
+    val fullName: String,
+    val street: String,
+    val phone: String,
+    val city: String,
+    val state: String
+): Parcelable {
+
+    constructor(): this("","","","","","")
+}
---
+++ app/src/main/java/com/example/kelineyt/fragments/shopping/AddressFragment.kt
@@ -0,0 +1,81 @@
+package com.example.kelineyt.fragments.shopping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.kelineyt.data.Address
+import com.example.kelineyt.databinding.FragmentAddressBinding
+import com.example.kelineyt.util.Resource
+import com.example.kelineyt.util.hideBottomNavigationView
+import com.example.kelineyt.viewmodel.AddressViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+
+@AndroidEntryPoint
+class AddressFragment : Fragment() {
+    private lateinit var binding: FragmentAddressBinding
+    val viewModel by viewModels<AddressViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.addNewAddress.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.progressbarAddress.visibility = View.VISIBLE
+                    }
+                    is Resource.Success ->{
+                        binding.progressbarAddress.visibility = View.INVISIBLE
+                        findNavController().navigateUp()
+                    }
+                    is Resource.Error ->{
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.error.collectLatest {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddressBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            buttonSave.setOnClickListener {
+                val addressTitle = edAddressTitle.text.toString()
+                val fullName = edFullName.text.toString()
+                val street = edStreet.text.toString()
+                val phone = edPhone.text.toString()
+                val city = edCity.text.toString()
+                val state = edState.text.toString()
+                val address = Address(addressTitle, fullName, street, phone, city, state)
+
+                viewModel.addAddress(address)
+            }
+        }
+    }
+
+}
