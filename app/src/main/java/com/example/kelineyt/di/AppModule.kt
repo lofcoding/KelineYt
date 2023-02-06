af6ed4e084bafa5b368710eda68f96a1de6e9f48
mohammad <mohammednawas8@gmail.com>
2023-02-06 14:06:49 +0200

    E31

--- app/src/main/java/com/example/kelineyt/di/AppModule.kt
+++ app/src/main/java/com/example/kelineyt/di/AppModule.kt
@@ -8,6 +8,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -38,5 +39,7 @@ object AppModule {
         firestore: FirebaseFirestore
     ) = FirebaseCommon(firestore,firebaseAuth)
 
-
+    @Provides
+    @Singleton
+    fun provideStorage() = FirebaseStorage.getInstance().reference
 }
---
+++ app/src/main/java/com/example/kelineyt/viewmodel/UserAccountViewModel.kt
@@ -0,0 +1,145 @@
+package com.example.kelineyt.viewmodel
+
+import android.app.Application
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kelineyt.KelineApplication
+import com.example.kelineyt.data.User
+import com.example.kelineyt.util.RegisterValidation
+import com.example.kelineyt.util.Resource
+import com.example.kelineyt.util.validateEmail
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.StorageReference
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.io.ByteArrayOutputStream
+import java.util.UUID
+import javax.inject.Inject
+
+@HiltViewModel
+class UserAccountViewModel @Inject constructor(
+    private val firestore: FirebaseFirestore,
+    private val auth: FirebaseAuth,
+    private val storage: StorageReference,
+    app: Application
+) : AndroidViewModel(app) {
+
+    private val _user = MutableStateFlow<Resource<User>>(Resource.Unspecified())
+    val user = _user.asStateFlow()
+
+    private val _updateInfo = MutableStateFlow<Resource<User>>(Resource.Unspecified())
+    val updateInfo = _updateInfo.asStateFlow()
+
+    init {
+        getUser()
+    }
+
+    fun getUser() {
+        viewModelScope.launch {
+            _user.emit(Resource.Loading())
+        }
+
+        firestore.collection("user").document(auth.uid!!).get()
+            .addOnSuccessListener {
+                val user = it.toObject(User::class.java)
+                user?.let {
+                    viewModelScope.launch {
+                        _user.emit(Resource.Success(it))
+                    }
+                }
+            }.addOnFailureListener {
+                viewModelScope.launch {
+                    _user.emit(Resource.Error(it.message.toString()))
+                }
+            }
+    }
+
+    fun updateUser(user: User, imageUri: Uri?) {
+        val areInputsValid = validateEmail(user.email) is RegisterValidation.Success
+                && user.firstName.trim().isNotEmpty()
+                && user.lastName.trim().isNotEmpty()
+
+        if (!areInputsValid) {
+            viewModelScope.launch {
+                _user.emit(Resource.Error("Check your inputs"))
+            }
+            return
+        }
+
+        viewModelScope.launch {
+            _updateInfo.emit(Resource.Loading())
+        }
+
+        if (imageUri == null) {
+            saveUserInformation(user, true)
+        } else {
+            saveUserInformationWithNewImage(user, imageUri)
+        }
+
+    }
+
+    private fun saveUserInformationWithNewImage(user: User, imageUri: Uri) {
+        viewModelScope.launch {
+            try {
+                val imageBitmap = MediaStore.Images.Media.getBitmap(
+                    getApplication<KelineApplication>().contentResolver,
+                    imageUri
+                )
+                val byteArrayOutputStream = ByteArrayOutputStream()
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 96, byteArrayOutputStream)
+                val imageByteArray = byteArrayOutputStream.toByteArray()
+                val imageDirectory =
+                    storage.child("profileImages/${auth.uid}/${UUID.randomUUID()}")
+                val result = imageDirectory.putBytes(imageByteArray).await()
+                val imageUrl = result.storage.downloadUrl.await().toString()
+                saveUserInformation(user.copy(imagePath = imageUrl), false)
+            } catch (e: Exception) {
+                viewModelScope.launch {
+                    _user.emit(Resource.Error(e.message.toString()))
+                }
+            }
+        }
+    }
+
+    private fun saveUserInformation(user: User, shouldRetrievedOldImage: Boolean) {
+        firestore.runTransaction { transaction ->
+            val documentRef = firestore.collection("user").document(auth.uid!!)
+            if (shouldRetrievedOldImage) {
+                val currentUser = transaction.get(documentRef).toObject(User::class.java)
+                val newUser = user.copy(imagePath = currentUser?.imagePath ?: "")
+                transaction.set(documentRef, newUser)
+            } else {
+                transaction.set(documentRef, user)
+            }
+        }.addOnSuccessListener {
+            viewModelScope.launch {
+                _updateInfo.emit(Resource.Success(user))
+            }
+        }.addOnFailureListener {
+            viewModelScope.launch {
+                _updateInfo.emit(Resource.Error(it.message.toString()))
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
